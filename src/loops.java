177f4185bddbb9c03694f383c574adb88ded929e
usman.ghani <usman.ghani@techno-soft.com>
2022-06-30 11:40:23 +0500

    creating new repo

---
+++ src/loops.java
@@ -0,0 +1,26 @@
+public class loops {
+    // Loops practice here //
+
+
+
+    public static void main(String[] args)
+    {
+        int i = 0;
+
+        while(i < 5)
+        {
+            System.out.println("while loop");
+            i++;
+        }
+
+        for(int j = 0; j < 5; j++)
+        {
+            System.out.println("for loop");
+        }
+
+
+
+
+
+    }
+}
